8829a05763a74620542b053d5f7ac3b8bfb5290e
Louis Aurea <louis@nextstation.cl>
2025-06-26 19:12:38 -0400

    adding times

--- app/api/endpoints/call_saver.py
+++ app/api/endpoints/call_saver.py
@@ -1,18 +1,22 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
-
+import json
 router = APIRouter()
 
 @router.post("/webhooks/aircall")
 async def aircall_webhook(request: Request):
     try:
-        data = await request.json()
-        print("📞 Webhook recibido:")
-        print(data)  # Mostrar todos los datos del webhook
+        body = await request.body()
+        print("🔍 Raw body:")
+        print(body.decode("utf-8"))
+        data = json.loads(body)
+
+        print("✅ Webhook recibido:")
+        print(data)
 
         return JSONResponse(
             status_code=200,
-            content={"message": "✅ Webhook recibido correctamente"}
+            content={"message": "Webhook recibido correctamente"}
         )
     except Exception as e:
         print("❌ Error al procesar webhook:", e)
