9c7e957e5e54b5738cb80deb2d2e8d5569bb0d4e
Louis Aurea <louis@nextstation.cl>
2025-06-26 19:28:39 -0400

    addiing router

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-
+from app.api.endpoints.call_saver import router as router_call_saver
 from app.api.endpoints.ghl_contacts import router as router_ghl__contacts
 from app.api.endpoints.health_check import router as router_check    
 from app.api.endpoints.debug import router as router_debug
@@ -8,7 +8,9 @@ from app.api.endpoints.db_healthcheck import router as router_db_healthcheck
 
 api_router = APIRouter()
 
- 
+api_router.include_router(router_call_saver, tags=["aircall webhook"],
+    responses={404: {"description": "Not found"}})
+
 api_router.include_router(router_ghl__contacts, tags=["Ghl Contacts"],
     responses={404: {"description": "Not found"}})
  #routes debug
