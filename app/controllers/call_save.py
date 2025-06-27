56452c97358e389d1eb5ce795cb4d29dd1b7f272
Louis Aurea <louis@nextstation.cl>
2025-06-27 12:07:35 -0400

    fixing endpoint and controller

--- app/controllers/call_save.py
+++ app/controllers/call_save.py
@@ -6,7 +6,7 @@ from app.models.CallModel import CallModel
 from app.schemas.call_model import CallModelCreate
 from app.core.database import get_session
 
-class CallRecordController(CRUDBase[CallModel, CallModelCreate]):
+class CallSaveController(CRUDBase[CallModel, CallModelCreate]):
     async def save_call_from_webhook(self,call_id, first_name, last_name, phone_number):
         async for session in get_session():  # get_session es async generator
             call = CallModel(
@@ -20,9 +20,9 @@ class CallRecordController(CRUDBase[CallModel, CallModelCreate]):
 
     async def create_call_from_webhook(self,data: CallModelCreate, session:Session ):
         try:
-            create = await self.save_call_from_webhook(db=session, obj_in=data)
+            create = await self.create(db=session, obj_in=data)
             return create
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Hay un error:{str(e)}")
 
-call_save_controller = CallRecordController(CallModel)
+call_save_controller = CallSaveController(CallModel)
