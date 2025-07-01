d1fbc7c6d87427598746c2832318666eec2b9dd0
Louis Aurea <louis@nextstation.cl>
2025-07-01 11:58:06 -0400

    adding

---
+++ app/models/Contacts.py
@@ -0,0 +1,25 @@
+from uuid import uuid4
+from sqlalchemy import Column, DateTime, String, func
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.models.BaseModel import BaseModel
+
+class Contact(BaseModel):
+    __tablename__ = 'contact'
+
+    uuid = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4)
+    
+    contact_id = Column(String(50), nullable=False)
+    contact_name = Column(String(50), nullable=False)
+    create_date = Column(String(50), nullable=False)
+    asign_to = Column(String(50), nullable=False)  
+    phone_number = Column(String(50), nullable=False)
+    source = Column(String(50), nullable=False)
+    tags = Column(String(50), nullable=False)
+    custom_fields = Column(String(50), nullable=False)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    deleted_at = Column(DateTime, nullable=True, default=None)
+    
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,2 @@
 from app.models.CallModel import CallModel
+from app.models.Contacts import Contact
