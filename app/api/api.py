8d868e57f9e89470baf0eb6d8976cb697e46f1bd
Louis Aurea <louis@nextstation.cl>
2025-07-09 22:09:07 -0400

    some bug of call

--- app/api/api.py
+++ app/api/api.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.endpoints.calls import router as router_calls
-from app.api.endpoints.ghl_contacts import router as router_ghl__contacts
 from app.api.endpoints.health_check import router as router_check    
 
 api_router = APIRouter()
 
 api_router.include_router(router_calls, tags=["GHL webhook"],
-    responses={404: {"description": "Not found"}})
-
-api_router.include_router(router_ghl__contacts, tags=["Ghl Contacts"],
     responses={404: {"description": "Not found"}})
 
 #route Checkout
