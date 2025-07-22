ef152b6fd34ff9729f53040c82b7a4f735200014
Louis Aurea <louis@nextstation.cl>
2025-07-22 19:30:17 -0400

    adding create endpoit"

--- app/controllers/call.py
+++ app/controllers/call.py
@@ -2,15 +2,15 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 
 from app.services.base import CRUDBase
-from app.models.CallModel import CallModel
-from app.schemas.call_model import CallModelCreate,CallModelUpdate
+from app.models.CallCrm import CallCrm
+from app.schemas.call_crm import CallCrmCreate,CallCrmUpdate
 
-class CallController(CRUDBase[CallModel, CallModelCreate, CallModelUpdate]):
-    async def create_call(self, data: CallModelCreate, session: Session):
+class CallController(CRUDBase[CallCrm, CallCrmCreate, CallCrmUpdate]):
+    async def create_call(self, data: CallCrmCreate, session: Session):
         try:
             calls = self.create(db=session, obj_in=data)
             return calls
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Hay un error: {str(e)}")
     
-call_controller = CallController(CallModel)
+call_controller = CallController(CallCrm)
