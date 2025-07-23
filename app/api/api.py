f368be07048afd1ddf419e84d27bea721ddab1b1
Louis Aurea <louis@nextstation.cl>
2025-07-23 12:08:40 -0400

    adding endpoint

--- app/api/api.py
+++ app/api/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.endpoints.calls import router as router_calls
 from app.api.endpoints.health_check import router as router_check    
 from app.api.endpoints.debug import router as router_debug
+from app.api.endpoints.db_health_check import router as router_db_health_check
 
 api_router = APIRouter()
 
@@ -14,4 +15,7 @@ api_router.include_router(router_check, tags=["HealthCheck"],
     responses={404: {"description": "Not found"}})
 
 api_router.include_router(router_debug, tags=["Debug"],
+    responses={404: {"description": "Not found"}})
+
+api_router.include_router(router_db_health_check, tags=["Healthcheck DB"],
     responses={404: {"description": "Not found"}})
