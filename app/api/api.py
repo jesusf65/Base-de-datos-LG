95f10d643cd7e5d11f4146062bb1017a3d7552d2
luis <luis1233210e@gmail.com>
2025-07-31 02:16:06 -0400

    deleting models debug

--- app/api/api.py
+++ app/api/api.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from app.api.endpoints.calls import router as router_calls
 from app.api.endpoints.health_check import router as router_check    
-from app.api.endpoints.debug import router as router_debug
 
 api_router = APIRouter()
 
@@ -13,7 +12,5 @@ api_router.include_router(router_calls, tags=["GHL webhook"],
 api_router.include_router(router_check, tags=["HealthCheck"],
     responses={404: {"description": "Not found"}})
 
-api_router.include_router(router_debug, tags=["Debug"],
-    responses={404: {"description": "Not found"}})
 
 
