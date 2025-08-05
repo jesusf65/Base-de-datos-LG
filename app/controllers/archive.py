5878c52e237456a7bedaead4feb22398796d6479
Louis Aurea <louis@nextstation.cl>
2025-08-05 12:59:32 -0400

    adding true adf

--- app/controllers/archive.py
+++ app/controllers/archive.py
@@ -2,9 +2,16 @@ from typing import Dict
 
 def build_adf_xml(data: Dict[str, str]) -> str:
     return f"""<?xml version="1.0" encoding="utf-8"?>
+<?adf version="1.0"?>
 <adf>
   <prospect>
     <requestdate>{data["created_at"]}</requestdate>
+    <vendor>
+      <id>8127727@leadsprod.dealercenter.net</id>
+    </vendor>
+    <provider>
+      <name>LeadGrowthCo</name>
+    </provider>
     <customer>
       <contact>
         <name part="first" type="individual">{data["first_name"]}</name>
@@ -13,15 +20,11 @@ def build_adf_xml(data: Dict[str, str]) -> str:
         <email>{data["email"]}</email>
         <phone type="voice">{data["phone"]}</phone>
       </contact>
+      <comments>{data["comment_text"]}</comments>
     </customer>
-    <comment>
-      {data["comment_text"]}
-    </comment>
-    <provider>
-      <name>LeadGrowthCo</name>
-    </provider>
   </prospect>
 </adf>"""
+
 
 def get_lead_email_recipients():
     """Retorna la lista de emails donde se deben enviar los leads"""
