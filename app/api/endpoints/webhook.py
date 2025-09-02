811e890c82d595e793d9c41f631a3e8ba9c7b640
Luis <luis1233210e@gmail.com>
2025-09-01 23:07:49 -0400

    paginatiopn

--- app/api/endpoints/webhook.py
+++ app/api/endpoints/webhook.py
@@ -8,3 +8,8 @@ router = APIRouter()
 async def ghl_contacts():
     ghl_contacts = await ghl_controller.get_ghl_contacts()
     return ghl_contacts  
+
+@router.get("/contacts-pagination")
+async def contacts_pagination(page: int = 1, limit: int = 50):
+    contacts = await ghl_controller.pagination_ghl_contacts(page=page, limit=limit)
+    return contacts
--- app/controllers/ghl.py
+++ app/controllers/ghl.py
@@ -20,4 +20,14 @@ class GhlController:
             response.raise_for_status()  # Lanza una excepción si hay error
             return response.json()
 
+
+    async def pagination_ghl_contacts(self, page: int = 1, limit: int = 50):
+        url = f"https://rest.gohighlevel.com/v1/contacts/?page={page}&limit={limit}"
+        headers = {
+            "Authorization": f"Bearer {API_KEY_CRM}"
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()
+
 ghl_controller = GhlController()    
